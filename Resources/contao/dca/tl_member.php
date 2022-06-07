92c9a15beb52df4024f580c56ea67394773e0fe3
coastforge-rro <raphael.ross@kuestenschmiede.gmbh>
2022-06-07 17:57:31 +0200

    Remove tl_member dca

--- Resources/contao/dca/tl_member.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['cartId'] = 'unique';
-
-$GLOBALS['TL_DCA']['tl_member']['fields']['cartId'] = [
-    'sql'                     => "varchar(50) NOT NULL default ''",
-];
